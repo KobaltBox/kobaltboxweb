4fc28832739f34046fb23bbbcebf5e89c8619048
jackmccomiskey <jackmccomiskey@gmail.com>
2020-10-25 23:07:31 +0000

    Added pages and placeholder content, test of unity web player

--- portal/app.py
+++ portal/app.py
@@ -1,11 +1,24 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+#Changes to static file paths because of how flask and unity deal with js...
+
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='static',
+            template_folder='templates')
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/2dshooter')
+def shooterdemo():
+    return render_template('2dshooter.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
